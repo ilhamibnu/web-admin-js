0aa5cdaa74c95ca8bf4b8b4761fde1622aad9eb5
ilhamibnu <e41210178@student.polije.ac.id>
2023-12-31 20:30:28 +0700

    initial db

---
+++ backend/controller/Users.js
@@ -0,0 +1,21 @@
+import User from "../models/UserModel.js";
+
+export const getUsers = async (req, res) => {
+
+}
+
+export const getUserById = async (req, res) => {
+    
+}
+
+export const createUser = async (req, res) => {
+        
+}
+
+export const updateUser = async (req, res) => {
+            
+}
+
+export const deleteUser = async (req, res) => {
+                
+}
