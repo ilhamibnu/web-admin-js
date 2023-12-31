80ee643e4f235d24ad2bf5a4bd2ddc27193a9f1d
ilhamibnu <e41210178@student.polije.ac.id>
2023-12-31 21:35:51 +0700

    feat user controller

--- backend/controller/Products.js
+++ backend/controller/Products.js
@@ -1,14 +1,37 @@
 import Product from "../models/ProductModel.js";
 
 export const getProducts = async (req, res) => {
+    try {
+        const response = await Product.findAll();
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({message: error.message || "Something went wrong"});
+    }
 
 }
 
 export const getProductById = async (req, res) => {
+        
+        try{
+            try {
+                const response = await Product.findOne({
+                    where: {
+                        uuid: req.params.id
+                    }
+                });
+                res.status(200).json(response);
+            } catch (error) {
+                res.status(500).json({message: error.message || "Something went wrong"});
+            }
+    
+        }catch(error){
+            res.status(500).json({message: error.message || "Something went wrong"});
+        }
     
 }
 
 export const createProduct = async (req, res) => {
+    
         
 }
 
--- backend/controller/Users.js
+++ backend/controller/Users.js
@@ -1,21 +1,84 @@
 import User from "../models/UserModel.js";
+import argon2 from "argon2";
 
 export const getUsers = async (req, res) => {
-
+    try {
+        const response = await User.findAll();
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({message: error.message || "Something went wrong"});
+    }
 }
 
 export const getUserById = async (req, res) => {
     
+    try{
+        try {
+            const response = await User.findOne({
+                where: {
+                    id: req.params.id
+                }
+            });
+            res.status(200).json(response);
+        } catch (error) {
+            res.status(500).json({message: error.message || "Something went wrong"});
+        }
+
+    }catch(error){
+        res.status(500).json({message: error.message || "Something went wrong"});
+    }
 }
 
 export const createUser = async (req, res) => {
+   const {name, email, password, role} = req.body;
+    if(!name || !email || !password || !role){
+         return res.status(400).json({message: "Please provide all fields"});
+    }
+    try {
+        const user = await User.findOne({where: {email}});
+        if(user){
+            return res.status(400).json({message: "User with this email already exists"});
+        }
+        const hashedPassword = await argon2.hash(password);
+        const newUser = await User.create({name, email, password: hashedPassword, role});
+        res.status(200).json(newUser);
         
+    } catch (error) {
+        res.status(500).json({message: error.message || "Something went wrong"});
+    }
 }
 
+
 export const updateUser = async (req, res) => {
+    const {name, email, password, role} = req.body;
+    if(!name || !email || !password || !role){
+         return res.status(400).json({message: "Please provide all fields"});
+    }
+    try {
+        const user = await User.findOne({where: {email}});
+        if(user){
+            return res.status(400).json({message: "User with this email already exists"});
+        }
+        const hashedPassword = await argon2.hash(password);
+        const newUser = await User.create({name, email, password: hashedPassword, role});
+        res.status(200).json(newUser);
+        
+    } catch (error) {
+        res.status(500).json({message: error.message || "Something went wrong"});
+    }
             
 }
 
 export const deleteUser = async (req, res) => {
+    try {
+        const response = await User.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json({message: "User deleted successfully"});
+    } catch (error) {
+        res.status(500).json({message: error.message || "Something went wrong"});
+    }
                 
 }
