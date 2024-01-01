0e5ff8acf30b150fcb09044f98662c71f6206413
ilhamibnu <e41210178@student.polije.ac.id>
2024-01-01 10:46:02 +0700

    feat products controller

--- backend/controller/Products.js
+++ backend/controller/Products.js
@@ -31,14 +31,53 @@ export const getProductById = async (req, res) => {
 }
 
 export const createProduct = async (req, res) => {
-    
-        
+
+        const { name, price, userId } = req.body;
+        try {
+            const response = await Product.create({
+                name,
+                price,
+                userId
+            });
+            res.status(200).json({message: "Product created successfully"});
+        } catch (error) {
+            res.status(500).json({message: error.message || "Something went wrong"});
+        }
 }
 
 export const updateProduct = async (req, res) => {
+        
+        const { id } = req.params;
+        const { name,  } = req.body;
+        try {
+            const response = await Product.update({
+                name,
+                price,
+                userId
+            },{
+                where: {
+                    id: id
+                }
+            });
+            res.status(200).json({message: "Product updated successfully"});
+        } catch (error) {
+            res.status(500).json({message: error.message || "Something went wrong"});
+        }
             
 }
 
 export const deleteProduct = async (req, res) => {
+
+    const { id } = req.params;
+    try {
+        const response = await Product.destroy({
+            where: {
+                id: id
+            }
+        });
+        res.status(200).json({message: "Product deleted successfully"});
+    } catch (error) {
+        res.status(500).json({message: error.message || "Something went wrong"});
+    }
                 
 }
